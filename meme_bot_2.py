d38b5b763e565a2bc60ce0abf1e8a9ffd621a831
Bellollo <lorenzotoschigr@gmail.com>
2025-01-23 18:37:55 +0100

    Add files via upload

---
+++ meme_bot_2.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os, random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def meme_homemade(ctx):
+    with open('images/homemade/my_meme.jpg', 'rb') as meme:
+        picture = discord.File(meme)
+   
+    await ctx.send(file=picture)
+
+@bot.command()
+async def normal_meme(ctx):
+    image = random.choice(os.listdir('images/normal'))
+    with open(f'images/{image}', 'rb') as meme:
+        picture = discord.File(meme)
+   
+    await ctx.send(file=picture)
+
+bot.run('token')
